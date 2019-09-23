c2795eb49ae4c70b554baaa71c3203b696189d34
Dmitry Morozov <dvmorozov@hotmail.com>
2019-09-23 19:12:41 +0200

    GPL was replaced by MPL.
    https://github.com/dvmorozov/fitminimizers/issues/1

--- SelfCheckedComponentList.pas
+++ SelfCheckedComponentList.pas
@@ -1,10 +1,11 @@
-{------------------------------------------------------------------------------
-    This software is distributed under GPL in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+{------------------------------------------------------------------------------------------------------------------------
+    This software is distributed under MPL 2.0 https://www.mozilla.org/en-US/MPL/2.0/ in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR ANY PARTICULAR PURPOSE.
 
-    Copyright (C) D.Morozov (dvmorozov@hotmail.com)
-------------------------------------------------------------------------------}
+    Copyright (C) Dmitry Morozov: dvmorozov@hotmail.com
+                        LinkedIn: https://www.linkedin.com/in/dmitry-morozov-79490a59/
+                        Facebook: https://www.facebook.com/dmitry.v.morozov
+------------------------------------------------------------------------------------------------------------------------}
 unit SelfCheckedComponentList;
 
 {$IFDEF Lazarus}
