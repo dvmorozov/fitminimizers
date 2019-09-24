d3dbe5657dfc604a49c6f41b327884c0ce39717c
Dmitry Morozov <7559490+dvmorozov@users.noreply.github.com>
2019-09-24 21:50:51 +0200

    Procedure GenerateRandomPointCloud has been implemented.
    https://github.com/dvmorozov/fitminimizers/issues/3

--- ExamplesMain.pas
+++ ExamplesMain.pas
@@ -8,15 +8,25 @@ uses
     System.SysUtils, System.Variants, System.Classes,
     Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
   {$ELSE}
-    SysUtils, Variants, Classes, Graphics, Controls, Forms, Dialogs,
+    SysUtils, Variants, Classes, Graphics, Controls, Forms, Dialogs, Buttons,
+    Contnrs,
   {$ENDIF}
-    Algorithm, DownhillSimplexAlgorithm;
+    Algorithm, DownhillSimplexAlgorithm, SimpMath;
 
 type
+    { TForm1 }
+
     TForm1 = class(TForm)
+      BitBtn1: TBitBtn;
         DownhillSimplexAlgorithm1: TDownhillSimplexAlgorithm;
+        procedure BitBtn1Click(Sender: TObject);
     private
         { Private declarations }
+        { Minimum bounding box problem. }
+        { Set of random points. }
+        PointCloud: TComponentList;
+
+        procedure GenerateRandomPointCloud;
     public
         { Public declarations }
     end;
@@ -27,5 +37,41 @@ var
 implementation
 
 {$R *.dfm}
+
+{ TForm1 }
+
+procedure TForm1.BitBtn1Click(Sender: TObject);
+begin
+    GenerateRandomPointCloud;
+end;
+
+procedure TForm1.GenerateRandomPointCloud;
+const PointCount: LongInt = 10;     //  Number of points in the cloud.
+//  Cloud boundaries.
+const MaxX: double = 10.0;
+const MinX: double = -10.0;
+const MaxY: double = 10.0;
+const MinY: double = -10.0;
+const MaxZ: double = 10.0;
+const MinZ: double = -10.0;
+var i: LongInt;
+    Point: T3DVector;
+begin
+    Randomize;
+    if PointCloud <> nil then
+        PointCloud.Destroy;
+
+    PointCloud := TComponentList.Create(True);
+
+    for i := 0 to PointCount - 1 do
+    begin
+        Point := T3DVector.Create(nil);
+        Point.Comps[0] := MinX + Random * (MaxX - MinX);
+        Point.Comps[1] := MinY + Random * (MaxY - MinY);
+        Point.Comps[2] := MinZ + Random * (MaxZ - MinZ);
+
+        PointCloud.Add(Point);
+    end;
+end;
 
 end.
--- examplesmain.dfm
+++ examplesmain.dfm
@@ -1,20 +1,28 @@
 object Form1: TForm1
   Left = 310
+  Height = 382
   Top = 132
+  Width = 532
   Caption = 'Form1'
   ClientHeight = 382
   ClientWidth = 532
   Color = clBtnFace
-  Font.Charset = DEFAULT_CHARSET
+  DesignTimePPI = 120
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
-  Font.Style = []
-  OldCreateOrder = True
-  PixelsPerInch = 96
-  TextHeight = 13
+  LCLVersion = '1.8.4.0'
+  object BitBtn1: TBitBtn
+    Left = 119
+    Height = 38
+    Top = 154
+    Width = 94
+    Caption = 'BitBtn1'
+    OnClick = BitBtn1Click
+    TabOrder = 0
+  end
   object DownhillSimplexAlgorithm1: TDownhillSimplexAlgorithm
-    Left = 112
-    Top = 16
+    left = 112
+    top = 16
   end
 end
