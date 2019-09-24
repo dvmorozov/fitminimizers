cdcf335899f6c13df2ece013bef9b08c98ee7042
Dmitry Morozov <dvmorozov@hotmail.com>
2019-09-24 12:55:15 +0200

    Sources have been formatted.
    https://github.com/dvmorozov/fitminimizers/issues/2

--- ExamplesMain.pas
+++ ExamplesMain.pas
@@ -4,25 +4,25 @@ interface
 
 uses
   {$IFNDEF Lazarus}
-  Winapi.Windows, Winapi.Messages,
-  System.SysUtils, System.Variants, System.Classes,
-  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+    Winapi.Windows, Winapi.Messages,
+    System.SysUtils, System.Variants, System.Classes,
+    Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
   {$ELSE}
-  SysUtils, Variants, Classes, Graphics, Controls, Forms, Dialogs,
+    SysUtils, Variants, Classes, Graphics, Controls, Forms, Dialogs,
   {$ENDIF}
-  Algorithm, DownhillSimplexAlgorithm;
+    Algorithm, DownhillSimplexAlgorithm;
 
 type
-  TForm1 = class(TForm)
-    DownhillSimplexAlgorithm1: TDownhillSimplexAlgorithm;
-  private
-    { Private declarations }
-  public
-    { Public declarations }
-  end;
+    TForm1 = class(TForm)
+        DownhillSimplexAlgorithm1: TDownhillSimplexAlgorithm;
+    private
+        { Private declarations }
+    public
+        { Public declarations }
+    end;
 
 var
-  Form1: TForm1;
+    Form1: TForm1;
 
 implementation
 
