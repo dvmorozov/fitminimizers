34f7d39a3eb5601ca95e57cfdfd7937e494031da
Dmitry Morozov <dvmorozov@hotmail.com>
2019-10-11 11:46:58 +0200

    Main package module was regenerated.
    https://github.com/dvmorozov/fitminimizers/issues/11

--- package/FitMinimizers.pas
+++ package/FitMinimizers.pas
@@ -1,25 +1,23 @@
 { This file was automatically created by Lazarus. Do not edit!
-  This source is only used to compile and install the package.
+This source is only used to compile and install the package.
  }
 
-unit FitMinimizers;
+unit FitMinimizers; 
 
-{$warn 5023 off : no warning about unused units}
 interface
 
 uses
-    Tools, SimpMath, MyExceptions, CBRCComponent, Algorithm, 
+  Tools, SimpMath, MyExceptions, CBRCComponent, Algorithm, 
     SelfCheckedComponentList, Decisions, DownhillSimplexAlgorithm, 
-    DownhillSimplexContainer, AlgorithmContainer, RunningThread, 
-    CombEnumerator, LazarusPackageIntf;
+    LazarusPackageIntf; 
 
 implementation
 
-procedure Register;
+procedure Register; 
 begin
-  RegisterUnit('DownhillSimplexAlgorithm', @DownhillSimplexAlgorithm.Register);
-end;
+  RegisterUnit('DownhillSimplexAlgorithm', @DownhillSimplexAlgorithm.Register); 
+end; 
 
 initialization
-  RegisterPackage('FitMinimizers', @Register);
+  RegisterPackage('FitMinimizers', @Register); 
 end.
