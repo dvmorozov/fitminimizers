f4c0a22420e685141a6ac863c22681e7ed43ec33
Dmitry Morozov <7559490+dvmorozov@users.noreply.github.com>
2019-12-11 13:38:06 +0100

    Version number has been added.
    https://github.com/dvmorozov/fitminimizers/issues/4

--- package/FitMinimizers.pas
+++ package/FitMinimizers.pas
@@ -1,26 +1,24 @@
 { This file was automatically created by Lazarus. Do not edit!
-  This source is only used to compile and install the package.
+This source is only used to compile and install the package.
  }
 
-unit FitMinimizers;
+unit FitMinimizers; 
 
-{$warn 5023 off : no warning about unused units}
 interface
 
 uses
-    Tools, SimpMath, MyExceptions, CBRCComponent, Algorithm, 
+  Tools, SimpMath, MyExceptions, CBRCComponent, Algorithm, 
     SelfCheckedComponentList, Decisions, DownhillSimplexAlgorithm, 
     DownhillSimplexContainer, AlgorithmContainer, RunningThread, 
-    CombEnumerator, LazarusPackageIntf;
+    CombEnumerator, LazarusPackageIntf; 
 
 implementation
 
-procedure Register;
+procedure Register; 
 begin
-  RegisterUnit('DownhillSimplexAlgorithm', @DownhillSimplexAlgorithm.Register);
-  RegisterUnit('RunningThread', @RunningThread.Register);
-end;
+  RegisterUnit('DownhillSimplexAlgorithm', @DownhillSimplexAlgorithm.Register); 
+end; 
 
 initialization
-  RegisterPackage('FitMinimizers', @Register);
+  RegisterPackage('FitMinimizers', @Register); 
 end.
