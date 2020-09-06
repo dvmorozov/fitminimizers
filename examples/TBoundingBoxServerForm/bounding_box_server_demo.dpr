7ae0d917bb56f8c4d6a4ca26af52bea3ec5ad17f
Dmitry Morozov <7559490+dvmorozov@users.noreply.github.com>
2020-09-06 11:01:32 +0200

    Bug in Delphi version fixed.
    #17

--- examples/TBoundingBoxServerForm/bounding_box_server_demo.dpr
+++ examples/TBoundingBoxServerForm/bounding_box_server_demo.dpr
@@ -3,7 +3,9 @@ program bounding_box_server_demo;
 uses
   Vcl.Forms,
   bounding_box_form in 'bounding_box_form.pas' {Form1},
-  bounding_box_server in 'bounding_box_server.pas';
+  bounding_box_server in 'bounding_box_server.pas',
+  optimizing_app in '..\optimizing_app.pas',
+  int_user_interaction in '..\int_user_interaction.pas';
 
 {$R *.res}
 
@@ -11,5 +13,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TBoundingBoxForm, BoundingBoxForm);
+  OptimizingApp := TOptimizingApp.Create(nil);
   Application.Run;
 end.
